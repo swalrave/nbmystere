b39c3bde99f530626dc9a61a10bc50fd0d041ac0
swalrave <59697555+swalrave@users.noreply.github.com>
2021-07-24 15:17:22 +0200

    Update ComputerGuess.java

--- src/ComputerGuess.java
+++ src/ComputerGuess.java
@@ -2,87 +2,80 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ComputerGuess {
+	
+	int[] tabmin = {0,0,0,0};
+	int[] tabmax = {10,10,10,10};
+	int[] tabguess = {0,0,0,0};
+	Random rand = new Random();
+	
 	public ComputerGuess() {
 		//scanner pour les input
+		@SuppressWarnings("resource")
 		Scanner sc= new Scanner(System.in);
 		
-		//input
-		System.out.println("Entrer un nombre a 4 chiffres: ");
-		String nbr = sc.nextLine();
-	
-		//appel du constructeur separant les 4 chiffres (etant un string) en 4 variables distinctes
-		Cassage c = new Cassage(nbr);
-		
-		//separation des chiffres
-		c.separ(nbr);
+		//appel du constructeur de nbr aleatoire de l'ordinateur
+		ComputerNumber comp = new ComputerNumber();
+		//init du nbr d'input
+		String nbr;
 		
 		//nombre de tentatives
 		int tentative = 1;
-
-		//appel du constructeur de nbr aleatoire de l'ordinateur
-		ComputerNumber comp = new ComputerNumber();
 		
 		Boolean testWin = false;
-		//boucle tant que le resultat n'est pas trouvÈ ou que le compteur est inf a 10 (TODO)
+		
+		//input
+		
+		boolean setup = true;
 		do {
-			
-			//comparaison des chiffres avec les valeurs des var du nbr aleatoire du joueur
-			printRes milliers = new printRes(comp.ranMil, c.mil);
-			printRes centaines = new printRes(comp.ranCen, c.cen);
-			printRes dizaines = new printRes(comp.ranDiz, c.diz);
-			printRes unites = new printRes(comp.ranUni, c.uni);
-	
-			//conversion en string pour l'affichage des +, - et =
-			String affMilliers = milliers.affichage;
-			String affCentaines = centaines.affichage;
-			String affDizaines = dizaines.affichage;
-			String affUnites = unites.affichage;
-			
-			tentative ++;
-
-			ComputerGuessing cgMil = new ComputerGuessing(comp.ranMil, c.mil);
-			ComputerGuessing cgCen = new ComputerGuessing(comp.ranCen, c.cen);
-			ComputerGuessing cgDiz = new ComputerGuessing(comp.ranDiz, c.diz);
-			ComputerGuessing cgUni = new ComputerGuessing(comp.ranUni, c.uni);
-			comp.ranMil = cgMil.newInt;
-			comp.ranCen = cgCen.newInt;
-			comp.ranDiz = cgDiz.newInt;
-			comp.ranUni = cgUni.newInt;
-			
-			System.out.println("" + comp.ranMil + comp.ranCen + comp.ranDiz + comp.ranUni);
-			
-			if(tentative >= 11) {
-				break;
+			System.out.println("Entrer un nombre a 4 chiffres que l'ordinateur va trouver : ");
+			nbr = sc.nextLine();
+			try {
+				if(nbr.length() == 4  ) {  //v√©rification longeur de 4
+					Integer.parseInt(nbr); //v√©rification que ce sont des nombre
+					comp.setnumbertoguess(nbr);
+					setup = false;
+				}
+				else {
+					throw new NumberFormatException();
+				}
 			}
-			
-			if(((c.mil == comp.ranMil) && (c.cen == comp.ranCen) && (c.diz == comp.ranDiz) && (c.uni == comp.ranUni))) {
-				testWin = true;
-				break;
+			catch(NumberFormatException ex){
+				System.out.println("input invalide, veuillez r√©essayer.");
 			}
+		} while(setup);
 			
-			//affichage du 'resultat' sous forme de +, - et =
-			System.out.println(affMilliers + affCentaines + affDizaines + affUnites);
-		} while(testWin != true || (tentative < 11));
+		//boucle tant que le resultat n'est pas trouv√© ou que le compteur est inf a 10
+		do {
+			tabguess[0] = rand.nextInt(tabmax[0]) + tabmin[0];
+			tabguess[1] = rand.nextInt(tabmax[1]) + tabmin[1];
+			tabguess[2] = rand.nextInt(tabmax[2]) + tabmin[2];
+			tabguess[3] = rand.nextInt(tabmax[3]) + tabmin[3];
+			String guess = Integer.toString(tabguess[0]) +
+					Integer.toString(tabguess[1]) +
+					Integer.toString(tabguess[2]) +
+					Integer.toString(tabguess[3]) ;
+			String result = comp.guess(guess);
+			
+			System.out.println("tentative : " + tentative);
+			System.out.println(guess);
+			
+			tentative++;
+			
+			analyse(result);
+			
+		} while(testWin == false & (tentative < 11));
 		
 		if(testWin == false) {
-			System.out.println("L'ordinateur a perdu au bout de la dixiËme tentative. Votre rÈponse Ètait: " + nbr);
-		}
-		
-		else {
-			System.out.println("L'ordinateur a gagnÈ !");
-		}
-		
-		System.out.println("1. rejouer -- 2. Menu -- 3. Quitter");
-		int choix = sc.nextInt();
-		
-		if(choix == 1) {
-			ComputerGuess game = new ComputerGuess();
-		}
-		if(choix == 2) {
-			nombremystere n = new nombremystere();
+			System.out.println("Bravo. l'ordinateur n'a pas deviner en 10 tentative.");
 		}
 		else {
-			System.out.println("Fin du programme.");
+			System.out.println("l'ordinateur a deviner en " + tentative + " tentative.");
 		}
 	}
+	
+	private void analyse(String result){
+		//TODO
+		//peut gerer le random avec tabmin, tabmax et tabguess
+	}
+	
 }
