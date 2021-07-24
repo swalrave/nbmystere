773ae70eef42ccd2df7b76fc967796eb0f7b3ac6
swalrave <59697555+swalrave@users.noreply.github.com>
2021-07-24 15:17:01 +0200

    Update PlayerGuess.java

--- src/PlayerGuess.java
+++ src/PlayerGuess.java
@@ -1,81 +1,54 @@
 import java.util.Scanner;
 
 public class PlayerGuess {
+	
 	public PlayerGuess() {
 		//scanner pour les input
+		@SuppressWarnings("resource")
 		Scanner sc= new Scanner(System.in);
 		
 		//appel du constructeur de nbr aleatoire de l'ordinateur
 		ComputerNumber comp = new ComputerNumber();
-		
 		//init du nbr d'input
-		String nbr = "0000";
-		
-		//appel du constructeur separant les 4 chiffres (etant un string) en 4 variables distinctes
-		Cassage c = new Cassage(nbr);
-		
-		// CrÈa de l'affichage du resultat
-		String ranAffich = "" + comp.ranMil + comp.ranCen + comp.ranDiz + comp.ranUni;
+		String nbr;
 		
 		//nombre de tentatives
 		int tentative = 1;
 		
 		Boolean testWin = false;
-		//boucle tant que le resultat n'est pas trouvÈ ou que le compteur est inf a 10 (TODO)
+		//boucle tant que le resultat n'est pas trouv√© ou que le compteur est inf a 10
 		do {
 			//input
-			System.out.println("tentative n∞ " + tentative + " - Entrer un nombre a 4 chiffres: ");
+			System.out.println("tentative n¬∞ " + tentative + " - Entrer un nombre a 4 chiffres: ");
+			
 			nbr = sc.nextLine();
 			
-			//separation des chiffres
-			c.separ(nbr);
-			
-			//comparaison des chiffres avec les valeurs des var du nbr aleatoire du pc
-			printRes milliers = new printRes(c.mil, comp.ranMil);
-			printRes centaines = new printRes(c.cen, comp.ranCen);
-			printRes dizaines = new printRes(c.diz, comp.ranDiz);
-			printRes unites = new printRes(c.uni, comp.ranUni);
-	
-			//conversion en string pour l'affichage des +, - et =
-			String affMilliers = milliers.affichage;
-			String affCentaines = centaines.affichage;
-			String affDizaines = dizaines.affichage;
-			String affUnites = unites.affichage;
-			
-			tentative ++;
-			
-			if(tentative >= 11) {
-				break;
+			try {
+				if(nbr.length() == 4  ) {  //v√©rification longeur de 4
+					Integer.parseInt(nbr); //v√©rification que ce sont des nombre
+					tentative ++;
+					String result = comp.guess(nbr);
+					System.out.println(result);
+					if(result.equals("====")) {
+						testWin = true;
+					}
+				}
+				else {
+					throw new NumberFormatException();
+				}
+			}
+			catch(NumberFormatException ex){
+				System.out.println("input invalide, veuillez r√©essayer.");
 			}
 			
-			if(((c.mil == comp.ranMil) && (c.cen == comp.ranCen) && (c.diz == comp.ranDiz) && (c.uni == comp.ranUni))) {
-				testWin = true;
-				break;
-			}
-			
-			//affichage du 'resultat' sous forme de +, - et =
-			System.out.println(affMilliers + affCentaines + affDizaines + affUnites);
-		} while(testWin != true || (tentative < 11));
+		} while(testWin == false & (tentative < 11));
 		
 		if(testWin == false) {
-			System.out.println("Vous avez perdu au bout de la dixiËme tentative. La rÈponse Ètait: " + ranAffich);
-		}
-		
-		else {
-			System.out.println("Vous avez gagnÈ.");
-		}
-		
-		System.out.println("1. rejouer -- 2. Menu -- 3. Quitter");
-		int choix = sc.nextInt();
-		
-		if(choix == 1) {
-			PlayerGuess game = new PlayerGuess();
-		}
-		if(choix == 2) {
-			nombremystere n = new nombremystere();
+			System.out.println("Vous avez perdu au bout de la dixi√®me tentative. La r√©ponse √©tait: " + comp.generated());
 		}
 		else {
-			System.out.println("Fin du programme.");
+			System.out.println("Vous avez gagn√©.");
 		}
+		
 	}
 }
